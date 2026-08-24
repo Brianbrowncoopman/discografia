27fdb0fec5cc9401578d6c77bc2dd1043416866b
Tu Nombre <brianbrowncoopman@gmail.com>
2026-08-24 19:26:25 -0400

    correccion

--- src/main/java/org/iplacex/proyectos/discografia/artistas/Artista.java
+++ src/main/java/org/iplacex/proyectos/discografia/artistas/Artista.java
@@ -2,6 +2,7 @@ package org.iplacex.proyectos.discografia.artistas;
 
 import java.util.List;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.annotation.Id;
 
 
@@ -11,9 +12,13 @@ public class Artista {
     @Id 
     public String _id; //id string
 
+    @Field("nombre")
     public String nombre; //nombre en string
+    @Field("estilos")
     public List<String> estilos;  // estilos musicales, es una lista almacena varios  en string
    
+    @Field("anioFundacion")
     public int anioFundacion; // año de fundacion  en entero
+    @Field("estaActivo")
     public boolean estaActivo; // estaactivo booleano si o no
 }
--- src/main/java/org/iplacex/proyectos/discografia/discos/Disco.java
+++ src/main/java/org/iplacex/proyectos/discografia/discos/Disco.java
@@ -3,6 +3,7 @@ package org.iplacex.proyectos.discografia.discos;
 import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "discos")
 public class Disco {
@@ -10,8 +11,12 @@ public class Disco {
     @Id 
     public String _id;  //string id
 
+    @Field("idArtista")
     public String idArtista; //id artista ( de la clase artista)
+    @Field("nombre")
     public String nombre; //string nombre de la clase artista)
+    @Field("anioLanzamiento")
     public int anioLanzamiento; // string año
+    @Field("canciones")
     public List<String> canciones; // lista que almacena las canciones String
 }
